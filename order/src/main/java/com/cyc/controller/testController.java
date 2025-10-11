c50e3ab62b246057274d99d7d366c56cde85f82f
陈驿晨 <chenyc@xindeco.com.cn>
2025-10-11 11:24:38 +0800

    改成springboot多模块配置

---
+++ order/src/main/java/com/cyc/controller/testController.java
@@ -0,0 +1,23 @@
+package src.main.java.com.cyc.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+
+@RefreshScope  // 开启配置动态刷新 配置修改后无需重启服务
+@RestController
+public class testController {
+
+
+    // 使用@Value获取配置
+    @Value("${app.message:默认消息}")
+    private String message;
+
+    @RequestMapping("/test")
+    public String testGet(){
+        return message;
+    }
+}
---
+++ order/src/main/java/com/cyc/webDemoApplication.java
@@ -0,0 +1,11 @@
+package src.main.java.com.cyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class webDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(webDemoApplication.class, args);
+    }
+}
